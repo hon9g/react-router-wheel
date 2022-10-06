98177e0788248eea3b2e9218af3b3de614899b68
hon9g <mimmh93@gmail.com>
2022-10-07 01:48:17 +0900

    [feat] Route 컴포넌트는 현재 path가 자신의 path와 같으면 컴포넌트를 보여준다

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css'
 
+import { Route } from './router/react-router'
+
 import Home from './pages/Home'
 import About from './pages/About'
 
 function App() {
 
   return (
-    <div>
-      <Home />
-      <About />
-    </div>
+    <>
+      <Route path='/' component={<Home />} />
+      <Route path='/about' component={<About />} />
+    </>
   )
 }
 
--- src/router/react-router.tsx
+++ src/router/react-router.tsx
@@ -1,6 +1,19 @@
-import { ReactElement, ReactNode } from "react"
-import { Action as NavigationType, Location } from './router'
-import { Navigator } from "./context";
+import {
+    ReactElement,
+    ReactNode,
+    useMemo,
+} from "react"
+
+import {
+    Action as NavigationType,
+    Location,
+    stripBasename,
+} from './router'
+import {
+    Navigator,
+    NavigationContext,
+    LocationContext,
+} from "./context";
 
 export interface RouterProps {
     basename?: string;
@@ -10,21 +23,85 @@ export interface RouterProps {
     navigator: Navigator;
     static?: boolean;
 }
-export const Router = ({
+
+function Router ({
     basename: basenameProp = "/",
     children = null,
     location: locationProp,
     navigationType = NavigationType.Pop,
     navigator,
     static: staticProp = false,
-  }: RouterProps): ReactElement | null => {
-    return null
-  }
+  }: RouterProps): ReactElement | null {
+    // Preserve trailing slashes on basename, so we can let the user control
+    // the enforcement of trailing slashes throughout the app
+    let basename = basenameProp.replace(/^\/*/, "/");
+    let navigationContext = useMemo(
+        () => ({ basename, navigator, static: staticProp }),
+        [basename, navigator, staticProp]
+    );
+
+    if (typeof locationProp === "string") {
+        locationProp = { pathname: locationProp }
+    }
+
+    let {
+        pathname = "/",
+        search = "",
+        hash = "",
+        state = null,
+        key = "default",
+    } = locationProp;
+
+    let location = useMemo(() => {
+        let trailingPathname = stripBasename(pathname, basename);
+
+        if (trailingPathname == null) {
+        return null;
+        }
+
+        return {
+        pathname: trailingPathname,
+        search,
+        hash,
+        state,
+        key,
+        };
+    }, [basename, pathname, search, hash, state, key]);
+
+    if (location == null) {
+        console.warn(
+            `<Router basename="${basename}"> is not able to match the URL ` +
+            `"${pathname}${search}${hash}" because it does not start with the ` +
+            `basename, so the <Router> won't render anything.`
+        )
+        return null;
+    }
+
+    return (
+        <NavigationContext.Provider value={navigationContext}>
+        <LocationContext.Provider
+            children={children}
+            value={{ location, navigationType }}
+        />
+        </NavigationContext.Provider>
+    )
+}
+
+interface BrowserRouterProps {
+    children: ReactNode
+}
+
+export function createBrowserRouter({ children }: BrowserRouterProps): ReactElement {
+    return <>{ children }</>
+}
 
 interface RouteProps {
     path: string
     component: ReactElement
 }
 export const Route = ({ path, component }: RouteProps): ReactElement => {
-    return <></>
+    if (path !== location.pathname) {
+        return <></>
+    }
+    return component
 }
