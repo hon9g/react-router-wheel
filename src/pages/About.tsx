7e62fba961be03a08f77ddad775d151e3c343317
hon9g <mimmh93@gmail.com>
2022-10-06 15:58:14 +0900

    [feat] 보여주어야 할 화면 컴포넌트 추가

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>about</h1>
+            <button>go main</button>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>root</h1>
+            <button>about</button>
+        </div>
+    )
+}
+
+export default Home
